bfd3ba2945a9b410499cb7f34c9c87e483848b60
Kaii 카이 <89370178+SenorKaii@users.noreply.github.com>
2022-01-16 20:41:11 +0800

    Create GUI.java

---
+++ GUI.java
@@ -0,0 +1,14 @@
+
+/*
+This program display using JOptionPane
+*/
+import javax.swing.JOptionPane;
+public class MyProgram2
+{
+   public static void main(String[]args)
+   {
+        JOptionPane.showMessageDialog(null,"Rihanna - Work ");
+        JOptionPane.showMessageDialog(null, "WORK WORK WORK WORK WORK", "WORK" , JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+   }
+}
