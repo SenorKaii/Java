b9b4387447a6d224d51336345650c22f26f73f98
Kaii 카이 <89370178+SenorKaii@users.noreply.github.com>
2022-01-16 20:48:21 +0800

    Create EscapeChar.java

---
+++ EscapeChar.java
@@ -0,0 +1,13 @@
+
+//Using escape characters
+public  class EscapeChar2
+{
+    public static void main(String args [])
+    {
+      System.out.println("Programming is great fun!\n\tJAVA is great and \'Enjoy it!\'");
+      System.out.println("My address is:\n");     
+      System.out.print("\t386 Knoll Dr.\n\tFairfield, Ca\n\t94534-3120");
+
+   }
+}
+
