a88c13550b5f0fb4c0008af9a0710f0dfead4510
Kaii 카이 <89370178+SenorKaii@users.noreply.github.com>
2022-01-16 20:41:47 +0800

    Create starter.java

---
+++ starter.java
@@ -0,0 +1,10 @@
+
+//This is my First Java Program
+public class MyProgram1
+{
+    public static void main(String[]args)
+    {
+    System.out.println("Hello I'm The Handsomest Of Them All");
+        System.out.println("The Genius Calculus (Melancholia)");
+    }
+}
