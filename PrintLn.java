a5dd4d5e3d24f445d2f21e8d07061715680ad3a2
Kaii 카이 <89370178+SenorKaii@users.noreply.github.com>
2022-01-16 20:50:21 +0800

    Create PrintLn.java

---
+++ PrintLn.java
@@ -0,0 +1,14 @@
+
+//This is another simple Java Program
+public  class Lines
+{
+    public static void main(String args [])
+    {
+      System.out.println("Programming is great fun!");   //using println
+      System.out.print ("JAVA is great ");               //using print
+      System.out.println ("and I Love JAVA!");
+      System.out.print("My name is  ");
+      System.out.print(" Earl T. Wylie");
+
+   }
+}
